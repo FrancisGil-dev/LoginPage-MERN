3cb4c8ed471a0f426626415376491583e3e9ee2d
Francis.dev <149237151+FrancisGil-dev@users.noreply.github.com>
2024-01-30 07:10:23 +0800

    commit

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -7,6 +7,7 @@ export default function Login() {
   const showPass = () => {
     setShow(!show);
   }
+  //handle all the data
   return (
     <main className='border border-white h-auto w-[600px] text-white rounded-lg'>
         {/*Container*/}
@@ -26,11 +27,11 @@ export default function Login() {
               <h1>Password</h1>
               <div className='flex items-center'>
                 <input type = { show? "text" : "password"} required autoComplete='off' className='bg-transparent outline-none border border-white p-2 w-full '/>
-                <i className={`bi ${show ? "bi-eye" : "bi-eye-slash"} text-xl relative right-[35px] cursor-pointer`}onClick={showPass}></i>
+                <i className={`bi ${show ? "bi-eye" : "bi-eye-slash"} text-xl relative right-[35px] cursor-pointer transition-all`} onClick={showPass}></i>
               </div>
             </div>
             {/*Submit Button*/}
-            <div className='text-center w-[97%] flex flex-col gap-3'>
+            <div className='text-center w-[97%] flex flex-col gap-5'>
               <button className='p-2 border border-white w-full hover:bg-white hover:text-black transition-all' >Login</button>
               <div className='text-left'>
                 <h1>Doesn't have an account? <a className='underline' href='/register'>Register Here</a></h1>
